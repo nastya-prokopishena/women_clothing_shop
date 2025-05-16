d9e802087edce4ebe8f71ee3677c5f4354fff96c
DariaHrychyna <dariahrychyna@gmail.com>
2025-05-16 19:49:10 +0300

    add register login profile

--- app/domain/models/user.py
+++ app/domain/models/user.py
@@ -1,26 +1,43 @@
 from datetime import datetime
 from typing import List, Optional
+from bson import ObjectId
 
 
 class Address:
-    def __init__(self, street: str, city: str, postal_code: str, country: str, is_default: bool = False):
+    def __init__(self, street: str, city: str, postal_code: str, country: str, house: str, is_default: bool = False,
+                 _id=None):
+        self.house = house
         self.street = street
         self.city = city
         self.postal_code = postal_code
         self.country = country
-        self.is_default = is_default
+        # self.is_default = is_default
+        self._id = _id if _id else ObjectId()
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        _id = data.get("_id")
+        if _id and not isinstance(_id, ObjectId):
+            _id = ObjectId(_id)
+        return cls(street=data.get("street", ""), city=data.get("city", ""), postal_code=data.get("postal_code", ""),
+                   country=data.get("country", ""), house=data.get("house", ""), _id=_id)
 
 
 class User:
     def __init__(self, name: str, email: str, password_hash: str, phone: Optional[str] = None,
-                 addresses: Optional[List[Address]] = None, favorites: Optional[List[str]] = None,
-                 newsletter_sub: bool = False):
+                 surname: Optional[str] = None, addresses: Optional[List[Address]] = None,
+                 favorites: Optional[List[str]] = None, newsletter_sub: bool = False, reset_token: Optional[str] = None,
+                 reset_token_created_at: Optional[datetime] = None, _id=None):
         self.name = name
+        self.surname = surname
         self.email = email
         self.password_hash = password_hash
         self.phone = phone
         self.addresses = addresses or []
-        self.favorites = favorites or []
-        self.newsletter_sub = newsletter_sub
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        # self.favorites = favorites or []
+        # self.newsletter_sub = newsletter_sub
+        # self.created_at = datetime.utcnow()
+        # self.updated_at = datetime.utcnow()
+        self._id = _id if _id else ObjectId()
+        self.reset_token = reset_token
+        self.reset_token_created_at = reset_token_created_at
--- app/infrastructure/persistence/user_repository.py
+++ app/infrastructure/persistence/user_repository.py
@@ -9,7 +9,19 @@ class UserRepository:
 
     def insert(self, user: User):
         user_dict = user.__dict__.copy()
-        user_dict['addresses'] = [addr.__dict__ for addr in user.addresses]
+        user_dict['_id'] = user._id  # ObjectId напряму
+        user_dict['surname'] = user.surname
+        user_dict['addresses'] = [
+            {
+                'street': addr.street,
+                'city': addr.city,
+                'postal_code': addr.postal_code,
+                'country': addr.country,
+                'house': addr.house,
+                '_id': addr._id  # теж ObjectId напряму
+            } for addr in user.addresses
+        ]
+
         result = self.collection.insert_one(user_dict)
         user._id = result.inserted_id
         return user
@@ -18,13 +30,31 @@ class UserRepository:
         data = self.collection.find_one({"email": email})
         if not data:
             return None
-        addresses = [Address(**addr) for addr in data.get('addresses', [])]
+        addresses_raw = data.pop('addresses', [])
+        addresses = [Address.from_dict(addr) for addr in addresses_raw]
         return User(**data, addresses=addresses)
 
     def update(self, user: User):
         user_dict = user.__dict__.copy()
-        user_dict['addresses'] = [addr.__dict__ for addr in user.addresses]
-        self.collection.update_one({"_id": ObjectId(user._id)}, {"$set": user_dict})
+
+        user_dict['addresses'] = [
+            {
+                'street': addr.street,
+                'city': addr.city,
+                'postal_code': addr.postal_code,
+                'country': addr.country,
+                'house': addr.house,
+                '_id': addr._id  # ObjectId напряму
+            } for addr in user.addresses
+        ]
+
+        result = self.collection.update_one(
+            {"_id": user._id},  # вже ObjectId
+            {"$set": {"addresses": user_dict['addresses'], "phone": user.phone, "password_hash": user.password_hash,
+                      "surname": user.surname, "name": user.name,}})
+
+        if result.modified_count == 0:
+            print(f"[ERROR] Не оновлено: user_id={user._id}")
 
     def find_by_id(self, user_id: str) -> User:
         data = self.collection.find_one({"_id": ObjectId(user_id)})
